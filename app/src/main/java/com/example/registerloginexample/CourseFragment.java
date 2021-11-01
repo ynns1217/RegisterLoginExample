35d350030ef8df47ea665a4eeaf4e5ccec916dbe
oesnoey <ynns1217@gmail.com>
2021-11-02 01:51:16 +0900

    sy push

---
+++ app/src/main/java/com/example/registerloginexample/CourseFragment.java
@@ -0,0 +1,6 @@
+package com.example.registerloginexample;
+
+import androidx.fragment.app.Fragment;
+
+public class CourseFragment extends Fragment {
+}
--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,31 +1,43 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tv_id,tv_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
+        final Button courseButton = (Button) findViewById(R.id.courseButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
 
 
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
+        courseButton.setOnClickListener(new View.OnClickListener() {
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary_dark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new CourseFragment());
+                fragmentTransaction.commit();
 
+            }
+        });
 
     }
 }
