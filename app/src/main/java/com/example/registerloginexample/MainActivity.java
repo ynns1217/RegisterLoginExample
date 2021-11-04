b4fb87f20ad5f30b6305ac8b1df588410babb28a
sy78 <j.1013@hanmail.net>
2021-11-05 02:08:42 +0900

    main xml - java

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.registerloginexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        et_id = findViewById(R.id.et_id);
-        et_pass = findViewById(R.id.et_pass);
-
-
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
-
-        et_id.setText(userID);
-        et_pass.setText(userPass);
 
 
     }
