6a672c5bfe056f15628431e121718b9a8b23d68f
oesnoey <ynns1217@gmail.com>
2021-11-06 02:01:08 +0900

    main.java 도메인 검색이나 내 도메인 누르면 넘어갈때 화면 조정

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,43 +1,141 @@
 package com.example.registerloginexample;
 
-import android.os.Build;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        final Button DomainSearchBtn = (Button) findViewById(R.id.DomainSearchBtn);
-        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+        userID=getIntent().getStringExtra("userID");
 
 
-        DomainSearchBtn.setOnClickListener(new View.OnClickListener() {
+        final Button courseButton=(Button) findViewById(R.id.DomainSearchBtn);
+        final Button scheduleButton=(Button) findViewById(R.id.scheduleButton);
+        final LinearLayout notice=(LinearLayout)findViewById(R.id.notice);
 
-            @RequiresApi(api = Build.VERSION_CODES.M)
+        courseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                notice.setVisibility(View.GONE);//공지사항이 안보임
-                DomainSearchBtn.setBackgroundColor(getResources().getColor(R.color.black));
+            public void onClick(View v) {
+                courseButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary_dark));
 
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment, new DomainFragment());//프래그먼트를 바꿔줌
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new DialogFragment());
                 fragmentTransaction.commit();
             }
-
-
         });
 
+
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE);//공지사항부분이 보이지 않도록, fragment로 바꿔줄수 있게
+                courseButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary_dark));
+
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new ScheduleFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+        new BackgroundTask().execute();
+    }
+
+
+
+    //사이트 리스트
+    class BackgroundTask extends AsyncTask<Void, Void, String >
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute(){
+            //super.onPreExecute();
+            target="https://ynns1217.ivyro.net/CourseList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url=new URL(target);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder=new StringBuilder();
+                while((temp=bufferedReader.readLine())!=null)
+                {
+                    stringBuilder.append(temp+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values){
+            super.onProgressUpdate();
+        }
+
+
+
+    }
+
+    //뒤로가기 두번 누르면 종료
+
+    private long lastTimeBackPressed;
+
+
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis()-lastTimeBackPressed<1500){ //뒤로가기 한번 누르고 1.5초안에 한번 더 누르면 종료
+            finish();
+            return;
+        }
+        Toast.makeText(this,"'뒤로' 버튼을 한 번 더 눌러 종료합니다.",Toast.LENGTH_SHORT);
+        lastTimeBackPressed=System.currentTimeMillis();
     }
 }
+
+
